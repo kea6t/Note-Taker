97a60c4c10fb890bb70553f8bdb7cc058e466f53
kea6t <keasay26@gmail.com>
2022-08-05 17:47:49 -0400

    figuring out to show the notes on the browser

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,40 +2,31 @@ const router = require('express').Router();
 const {
     findById,
     createNewNotes,
-    deleteNotes
+    validateNotes
 } = require('../../lib/notes')
 const { unikId } = require('unik-id');
 const { notes } = require('../../db/notes.json');
 
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     // let results = notes;
     // if (req.query) {
     //   results = getNote(req.query, results);
     // }
     res.json(notes);
+    console.log(notes);
 });
 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     console.log(req.body);
     // set id based on what the next index of the array will be.
     req.body.id = unikId();
-
-    // if any data in req.body is incorrect, send 400 error back
-    if(!notes) {
-        res.status(400).send('The note is not properly formatted.');
-    } else {
-    const note = createNewNotes(req.body, notes);
-    res.json(note);}
-});
-
-router.delete('/notes/:id', (req, res) => {
-    var note = findById(parseInt(req.params.id));
-  if (note) {
-    deleteNotes(req.params.id);
-    res.send("ok");
-  } else {
-    res.status(400).send("record not found");
-  }
+    
+    // // if any data in req.body is incorrect, send 400 error back
+    // if(!validateNotes) {
+    //     res.status(400).send('The note is not properly formatted.');
+    // } else {
+    const results = createNewNotes(req.body, notes);
+    res.json(results);
 });
 
 module.exports = router;
