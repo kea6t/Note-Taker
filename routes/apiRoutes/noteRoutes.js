dd3ab9ff0d8bb1d62997f3fa86b32a319135c9a5
kea6t <keasay26@gmail.com>
2022-08-05 18:46:18 -0400

    figuring out to show the notes on the browser

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -7,7 +7,7 @@ const {
 const { unikId } = require('unik-id');
 const { notes } = require('../../db/notes.json');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     // let results = notes;
     // if (req.query) {
     //   results = getNote(req.query, results);
@@ -16,7 +16,7 @@ router.get('/api/notes', (req, res) => {
     console.log(notes);
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     console.log(req.body);
     // set id based on what the next index of the array will be.
     req.body.id = unikId();
